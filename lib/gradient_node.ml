b466a0b9d8f64680feaf0d5d82e4f666f208fadc
Anish Krishnan <anishtech1@gmail.com>
2024-03-30 17:55:19 -0400

    Added more operations to [Gradient_node.t]

--- lib/gradient_node.ml
+++ lib/gradient_node.ml
@@ -1,6 +1,12 @@
 open! Core
 
-type node = Constant | Add of t * t | Mul of t * t | Tanh of t
+type node =
+  | Constant
+  | Add of t * t
+  | Mul of t * t
+  | Pow of t * float
+  | Tanh of t
+  | Exp of t
 [@@deriving sexp_of]
 
 and t = { node : node; value : float; mutable gradient : float; label : string }
@@ -16,12 +22,36 @@ let ( + ) left right =
     label = Printf.sprintf "(%s + %s)" left.label right.label;
   }
 
+let ( - ) left right =
+  {
+    node = Add (left, { right with value = -1. *. right.value });
+    value = left.value -. right.value;
+    gradient = 0.;
+    label = Printf.sprintf "(%s - %s)" left.label right.label;
+  }
+
 let ( * ) left right =
   {
     node = Mul (left, right);
     value = left.value *. right.value;
     gradient = 0.;
     label = Printf.sprintf "(%s + %s)" left.label right.label;
+  }
+
+let ( ** ) left right =
+  {
+    node = Pow (left, right);
+    value = left.value ** right;
+    gradient = 0.;
+    label = Printf.sprintf "(%s ^ %f)" left.label right;
+  }
+
+let ( / ) left right =
+  {
+    node = Mul (left, right ** -1.);
+    value = left.value /. right.value;
+    gradient = 0.;
+    label = Printf.sprintf "(%s / %s)" left.label right.label;
   }
 
 let tanh t =
@@ -33,6 +63,14 @@ let tanh t =
     label = Printf.sprintf "(tanh %s)" t.label;
   }
 
+let exp t =
+  {
+    node = Exp t;
+    value = Float.exp t.value;
+    gradient = 0.;
+    label = Printf.sprintf "(exp %s)" t.label;
+  }
+
 let backwards t =
   t.gradient <- 1.;
   let rec helper t =
@@ -42,17 +80,21 @@ let backwards t =
         left.gradient <- left.gradient +. t.gradient;
         right.gradient <- right.gradient +. t.gradient;
         helper left;
-        helper right;
-        ()
+        helper right
     | Mul (left, right) ->
         left.gradient <- left.gradient +. (right.value *. t.gradient);
         right.gradient <- right.gradient +. (left.value *. t.gradient);
         helper left;
-        helper right;
-        ()
+        helper right
+    | Pow (x, k) ->
+        x.gradient <-
+          (x.gradient +. Float.(x.gradient * (k * (x.value ** (k - 1.)))));
+        helper x
     | Tanh x ->
         x.gradient <- x.gradient +. ((1. -. Float.square x.value) *. t.gradient);
-        helper x;
-        ()
+        helper x
+    | Exp x ->
+        x.gradient <- x.gradient +. (x.gradient *. x.value);
+        helper x
   in
   helper t
--- lib/gradient_node.mli
+++ lib/gradient_node.mli
@@ -4,6 +4,10 @@ type t [@@deriving sexp_of]
 
 val create : label:string -> value:float -> t
 val ( + ) : t -> t -> t
+val ( - ) : t -> t -> t
 val ( * ) : t -> t -> t
+val ( ** ) : t -> float -> t
+val ( / ) : t -> t -> t
 val tanh : t -> t
+val exp : t -> t
 val backwards : t -> unit
